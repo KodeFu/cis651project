35fe1241bf89f8e9507214668d2187ce4e20b03d
Mudit Vats <muditv@gmail.com>
2019-11-23 15:16:38 -0700

    Using spinner instead of listview for consistency. Also edit category in one view since it's cumbersome to admin edit and then click a button to get another view which then allows you to edit the item.

--- mvlab7/app/src/main/java/com/example/myapplication/AdministerGroup.java
+++ mvlab7/app/src/main/java/com/example/myapplication/AdministerGroup.java
@@ -3,12 +3,23 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
 
 public class AdministerGroup extends AppCompatActivity {
-
+    String[] categories = { "Clothing", "Groceries", "Dining", "Ride Share", "Entertainment", "Gifts", "Fuel / Gas", "Automobile", "Home Improvement", "Credit Cards"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_administer_group);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, categories);
+
+        Spinner category = (Spinner) findViewById(R.id.category);
+        category.setAdapter(adapter);
+
     }
 }
--- mvlab7/app/src/main/java/com/example/myapplication/SubmitReceipt.java
+++ mvlab7/app/src/main/java/com/example/myapplication/SubmitReceipt.java
@@ -3,12 +3,19 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SubmitReceipt extends AppCompatActivity {
+    String[] categories = { "Clothing", "Groceries", "Dining", "Ride Share", "Entertainment", "Gifts", "Fuel / Gas", "Automobile", "Home Improvement", "Credit Cards"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit_receipt);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, categories);
+        Spinner category = (Spinner) findViewById(R.id.category);
+        category.setAdapter(adapter);
     }
 }

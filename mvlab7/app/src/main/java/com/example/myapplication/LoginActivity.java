d2714153221aa9a54bf60279516e366668712da4
Mudit Vats <muditv@gmail.com>
2019-11-29 12:20:04 -0700

    Added ability to login with username/password. Registration using username and password also work. This will create a new User in Firebase which can then be used in the login screen. As a test user "blah@blah.com" and password "password" will log you in.

--- mvlab7/app/src/main/java/com/example/myapplication/LoginActivity.java
+++ mvlab7/app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -1,14 +1,24 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +32,39 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(myIntent);
             }
         });
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void onClickLogin(View view) {
+        EditText email = findViewById(R.id.et_username);
+        EditText password = findViewById(R.id.et_password);
+        if (email.getText().toString().matches("") || password.getText().toString().matches(""))
+        {
+            Toast.makeText(getApplicationContext(), "Enter both email and password", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnSuccessListener(this,
+                    new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            Log.d("debug", "onClickSignIn:success");
+                            Toast.makeText(getApplicationContext(), "Sign in succeeded.", Toast.LENGTH_SHORT).show();
+                            successfulLogin();
+                        }
+                    }).addOnFailureListener(this, new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.d("debug", "onClickSignIn Failure:" + e.getMessage());
+                    Toast.makeText(getApplicationContext(), "Sign in failure" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    public void successfulLogin()
+    {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
--- mvlab7/app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ mvlab7/app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -1,17 +1,38 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class RegisterActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText email;
+    private EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        email = (EditText) findViewById(R.id.et_username);
+        password = (EditText) findViewById(R.id.et_password);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
     }
 
     public void onClickUploadPhoto(View view) {
@@ -20,7 +41,35 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void onClickSaveProfile(View view) {
+        if (email.getText().toString().matches("") || password.getText().toString().matches(""))
+        {
+            Toast.makeText(getApplicationContext(), "Enter both email and password", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            createAccount(email.getText().toString(), password.getText().toString());
+        }
         Toast.makeText(getApplicationContext(), "onClickSaveProfile",
                 Toast.LENGTH_SHORT).show();
+    }
+
+    private void createAccount(String email, String password) {
+        Log.d("debug", "createAccount");
+        mAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(this,
+                new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Log.d("debug", "createUserWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Toast.makeText(getApplicationContext(), "Authentication succeeded.", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("debug", "createAccount Failure:" + e.getMessage());
+                FirebaseUser user = mAuth.getCurrentUser();
+                Toast.makeText(getApplicationContext(), "Failure" + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

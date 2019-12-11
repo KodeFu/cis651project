eecd3dcdea8e62b3b25c4abde578cfebe9ae7167
Mudit Vats <muditv@gmail.com>
2019-12-10 18:42:37 -0700

    Added ability to display and update DisplayName field.

--- mvlab7/app/src/main/java/com/example/myapplication/ProfileActivity.java
+++ mvlab7/app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -1,16 +1,33 @@
 package com.example.myapplication;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class ProfileActivity extends BaseActivity {
+import androidx.annotation.NonNull;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+
+public class ProfileActivity extends BaseActivity {
+    private FirebaseAuth mAuth;
+    EditText displayName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         buildNavDrawerAndToolbar();
+
+        displayName = findViewById(R.id.et_display_name);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        displayName.setText(user.getDisplayName());
     }
 
     public void onClickUploadPhoto(View view) {
@@ -19,8 +36,31 @@ public class ProfileActivity extends BaseActivity {
     }
 
     public void onClickSaveProfile(View view) {
-        // https://firebase.google.com/docs/auth/android/manage-users
-        Toast.makeText(getApplicationContext(), "onClickSaveProfile",
-                Toast.LENGTH_SHORT).show();
+
+        // FIXME: Update to use path of picture
+        String urlForPicture = "https://console.firebase.google.com/u/4/project/family-budget-df281/database/mypicture.jpg";
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user!=null) {
+            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                    .setDisplayName(displayName.getText().toString())
+                    .setPhotoUri(Uri.parse("http://example.com/supercontra.jpg"))
+                    .build();
+
+            user.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(ProfileActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        displayName.setText(user.getDisplayName());
+                    }
+                    else
+                    {
+                        Toast.makeText(ProfileActivity.this, "Profile Save Failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
